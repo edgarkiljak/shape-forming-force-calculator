0af6503864d79cea84499c50815d322c69111409
edgarkiljak <edgar.kiljak@adylic.com>
2020-12-17 15:18:27 +0000

    toggle button active class logic wip

--- src/components/slide-3/Slide3.js
+++ src/components/slide-3/Slide3.js
@@ -25,17 +25,17 @@ const Slide3 = (props) => {
 
   toggleButtons.map((e) => console.log(e));
 
-  // const toggleButton = toggleButtons.map((el, index) => {
-  //   return (
-  //     <ButtonToggle
-  //       key={index}
-  //       onClick={handleOnClick(index)}
-  //       className={activeIndex === index ? 'active' : null}
-  //       label={el[index].label}
-  //       side={el[index].side}
-  //     />
-  //   );
-  // });
+  const toggleButton = toggleButtons.map((el, index) => {
+    return (
+      <ButtonToggle
+        key={index}
+        onClick={() => handleOnClick(index)}
+        className={activeIndex === index ? 'active' : 'passive'}
+        label={el.label}
+        side={el.side}
+      />
+    );
+  });
   // Logos setup
   const logoStyle = {
     fontWeight: 'lighter',
@@ -88,7 +88,7 @@ const Slide3 = (props) => {
         />
         <Slider name="Length" id="length-slider" min={120} max={150} step={1} />
       </div>
-      {/* <div className="toggle-buttons-container">{toggleButton}</div> */}
+      <div className="toggle-buttons-container">{toggleButton}</div>
       <Logo position="bottom" headline="Brand ltda." />
       <ButtonWhite label="Next Step" icon={faArrowRight} />
     </div>
